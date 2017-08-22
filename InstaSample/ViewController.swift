f1822ecd1f0b55fa2bf28836af00491dd84d7eae
masuhara <info@masuhara.net>
2017-08-23 06:14:09 +0900

    Initial Commit

---
+++ InstaSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaSample
+//
+//  Created by Masuhara on 2017/08/23.
+//  Copyright © 2017年 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
