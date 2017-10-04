b5c63779d233b1b4353a0ad0c44824ae127776cb
masuhara <info@masuhara.net>
2017-10-04 10:31:01 +0900

    Add func to follow and remove

--- InstaSample/ShowUserViewController.swift
+++ InstaSample/ShowUserViewController.swift
@@ -15,6 +15,8 @@ class ShowUserViewController: UIViewController, UICollectionViewDataSource {
     
     var selectedUser: NCMBUser!
     
+    var followingInfo: NCMBObject?
+    
     var posts = [Post]()
     
     @IBOutlet var userImageView: UIImageView!
@@ -62,8 +64,11 @@ class ShowUserViewController: UIViewController, UICollectionViewDataSource {
         // ユーザーの投稿した写真の読み込み
         loadPosts()
         
-        // フォロー中かどうかの読み込み
+        // フォロー状態の読み込み
         loadFollowingStatus()
+        
+        // フォロー数の読み込み
+        loadFollowingInfo()
     }
 
     override func didReceiveMemoryWarning() {
@@ -95,10 +100,18 @@ class ShowUserViewController: UIViewController, UICollectionViewDataSource {
             } else {
                 for following in result as! [NCMBObject] {
                     let user = following.object(forKey: "following") as! NCMBUser
+                    
+                    // フォロー状態だった場合、ボタンの表示を変更
                     if self.selectedUser.objectId == user.objectId {
-                        self.followButton.setTitle("フォロー解除", for: .normal)
-                        self.followButton.setTitleColor(UIColor.red, for: .normal)
-                        self.followButton.borderColor = UIColor.red
+                        // 表示変更を高速化するためにメインスレッドで処理
+                        DispatchQueue.main.async {
+                            self.followButton.setTitle("フォロー解除", for: .normal)
+                            self.followButton.setTitleColor(UIColor.red, for: .normal)
+                            self.followButton.borderColor = UIColor.red
+                        }
+                        
+                        // フォロー状態を管理するオブジェクトを保存
+                        self.followingInfo = following
                         break
                     }
                 }
@@ -147,7 +160,92 @@ class ShowUserViewController: UIViewController, UICollectionViewDataSource {
         })
     }
     
-    @IBAction func follow() {
+    func loadFollowingInfo() {
+        // フォロー中
+        let followingQuery = NCMBQuery(className: "Follow")
+        followingQuery?.includeKey("user")
+        followingQuery?.whereKey("user", equalTo: selectedUser)
+        followingQuery?.countObjectsInBackground({ (count, error) in
+            if error != nil {
+                SVProgressHUD.showError(withStatus: error!.localizedDescription)
+            } else {
+                // 非同期通信後のUIの更新はメインスレッドで
+                DispatchQueue.main.async {
+                    self.followingCountLabel.text = String(count)
+                }
+            }
+        })
         
+        // フォロワー
+        let followerQuery = NCMBQuery(className: "Follow")
+        followerQuery?.includeKey("following")
+        followerQuery?.whereKey("following", equalTo: selectedUser)
+        followerQuery?.countObjectsInBackground({ (count, error) in
+            if error != nil {
+                SVProgressHUD.showError(withStatus: error!.localizedDescription)
+            } else {
+                DispatchQueue.main.async {
+                    // 非同期通信後のUIの更新はメインスレッドで
+                    self.followerCountLabel.text = String(count)
+                }
+            }
+        })
+    }
+    
+    @IBAction func follow() {
+        // すでにフォロー状態だった場合、フォロー解除
+        if let info = followingInfo {
+            info.deleteInBackground({ (error) in
+                if error != nil {
+                    SVProgressHUD.showError(withStatus: error!.localizedDescription)
+                } else {
+                    DispatchQueue.main.async {
+                        self.followButton.setTitle("フォローする", for: .normal)
+                        self.followButton.setTitleColor(UIColor.blue, for: .normal)
+                        self.followButton.borderColor = UIColor.blue
+                    }
+                    
+                    // フォロー状態の再読込
+                    self.loadFollowingStatus()
+                    
+                    // フォロー数の再読込
+                    self.loadFollowingInfo()
+                }
+            })
+        } else {
+            let displayName = selectedUser.object(forKey: "displayName") as? String
+            let message = displayName! + "をフォローしますか？"
+            let alert = UIAlertController(title: "フォロー", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                let object = NCMBObject(className: "Follow")
+                if let currentUser = NCMBUser.current() {
+                    object?.setObject(currentUser, forKey: "user")
+                    object?.setObject(self.selectedUser, forKey: "following")
+                    object?.saveInBackground({ (error) in
+                        if error != nil {
+                            SVProgressHUD.showError(withStatus: error!.localizedDescription)
+                        } else {
+                            self.loadFollowingStatus()
+                        }
+                    })
+                } else {
+                    // currentUserが空(nil)だったらログイン画面へ
+                    let storyboard = UIStoryboard(name: "SignIn", bundle: Bundle.main)
+                    let rootViewController = storyboard.instantiateViewController(withIdentifier: "RootNavigationController")
+                    UIApplication.shared.keyWindow?.rootViewController = rootViewController
+                    
+                    // ログイン状態の保持
+                    let ud = UserDefaults.standard
+                    ud.set(false, forKey: "isLogin")
+                    ud.synchronize()
+                }
+            }
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .default) { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }
+            alert.addAction(okAction)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
